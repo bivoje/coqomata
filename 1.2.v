163d79c44dc45977f12dcdcd476ab01f2995de96
bivoje <bivoje49@gmail.com>
2019-02-15 16:24:19 +0900

    Use Prop instead of bool in Language definition

--- 1.2.v
+++ 1.2.v
@@ -63,102 +63,194 @@ Proof. reflexivity. Qed.
 
 (* it's hard to reason on infinite set represented as in coq's standard library.
    intead, use boolean function representation.. *)
-Definition Lang := Str -> bool.
+Definition Lang := Str -> Prop.
 
-Local Open Scope bool_scope.
-
-
-Fixpoint eqb_str (s1 s2 : Str) : bool :=
-  match s1, s2 with
-  | [],     [] => true
-  | _ :: _, [] => false
-  | [], _ :: _ => false
-  | h1 :: t1, h2 :: t2 => Nat.eqb h1 h2 && eqb_str t1 t2
-  end.
-
-Fixpoint list2lang (strs : list Str) (w : Str) : bool :=
+Fixpoint list2lang (strs : list Str) (w : Str) : Prop :=
   match strs with
-  | [] => false
-  | s :: trs => eqb_str s w || list2lang trs w
+  | [] => False
+  | s :: trs => s = w \/ list2lang trs w
   end.
 
 Definition lang_sample : Lang := list2lang [[0]; [0;0]; [0;0;1]].
 
-Example lang_sample_match0 : lang_sample nil = false.
-Proof. reflexivity. Qed.
+Example lang_sample_match0 : ~ lang_sample nil.
+Proof.
+  unfold not, lang_sample. simpl. intro H. destruct H.
+  inversion H. destruct H. inversion H. inversion H.
+  destruct H. inversion H. inversion H0. destruct H. inversion H. exact H.
+Qed.
 
-Example lang_sample_match1 : lang_sample [0;0] = true.
-Proof. reflexivity. Qed.
-
+Example lang_sample_match1 : lang_sample [0;0].
+Proof.
+  unfold lang_sample. simpl.
+  right. left. reflexivity.
+Qed.
 
 Definition lang_an (c : nat) : Lang :=
-  fix f (w : Str) :=
-    match w with
-    | [] => true
-    | x :: v => Nat.eqb x c && f v
-    end.
+  fun w => exists n, times n [c] = w.
 
-Example an_lang_match0 : forall c, lang_an c empty_str = true.
-Proof. reflexivity. Qed.
+Example lang_an_match0 : forall c, lang_an c empty_str.
+Proof.
+  unfold lang_an.
+  exists 0.
+  reflexivity.
+Qed.
 
-Example an_lang_match1 : lang_an 0 [0;0;0;0] = true.
-Proof. reflexivity. Qed.
+Example lang_an_match1 : forall c, lang_an c [c;c;c;c].
+Proof.
+  unfold lang_an.
+  exists 4.
+  reflexivity.
+Qed.
 
+Definition lang_anbn (c1 c2 : nat) : Lang :=
+  fun (w : Str) => exists n, concat (times n [c1]) (times n [c2]) = w.
 
-Definition anbn_lang : Lang :=
-  fun (w : Str) =>
-    let n2 := (length w) in
-    if Nat.odd n2 then
-      false
-    else
-      let n := Nat.div2 n2 in
-      lang_an 0 (firstn n w) && lang_an 1(skipn n w).
+Example lang_anbn_match0 : forall c1 c2, lang_anbn c1 c2 empty_str.
+Proof.
+  intros. unfold lang_anbn.
+  exists 0. reflexivity.
+Qed.
 
-Example anbn_lang_match0 : anbn_lang nil = true.
-Proof. reflexivity. Qed.
+Example lang_anbn_match1 : lang_anbn 0 1 [0;0;0;0;1;1;1;1].
+Proof. exists 4. reflexivity. Qed.
 
-Example anbn_lang_match1 : anbn_lang [0;0;0;0;1;1;1;1] = true.
-Proof. reflexivity. Qed.
+Lemma times_length : forall (c : nat) (w : Str),
+  length (times c w) = c * length w.
+Proof.
+  intros. induction c; simpl; try reflexivity.
+  unfold times. simpl. rewrite app_length.
+  apply f_equal2_plus; try reflexivity.
+  apply IHc.
+Qed.
 
-Example anbn_lang_nomat1 : anbn_lang [0;0;0;0;1;1;1] = false.
-Proof. reflexivity. Qed.
+Require Import Even.
+
+Lemma double_even : forall n, even (n + n).
+Proof.
+  induction n.
+  - simpl. apply even_O.
+  - rewrite <- plus_n_Sm. simpl.
+    apply even_S. apply odd_S. exact IHn.
+Qed.
+
+Lemma lang_anbn_even_length : forall c1 c2 w,
+  lang_anbn c1 c2 w -> even (length w).
+Proof.
+  intros. unfold lang_anbn in H.
+  destruct H as [n H]. rewrite <- H.
+  rewrite app_length.
+  rewrite (times_length n [c1]). rewrite (times_length n [c2]).
+  simpl. apply double_even.
+Qed.
+
+Example lang_anbn_nomat1 : ~ lang_anbn 0 1 [0;0;0;0;1;1;1].
+Proof.
+  unfold not. intros H.
+  pose (H0 := lang_anbn_even_length 0 1 [0;0;0;0;1;1;1]).
+  apply H0 in H. simpl in H.
+  inversion H; inversion H2; inversion H4; inversion H6;
+  inversion H8; inversion H10; inversion H12; inversion H14.
+Qed.
 
 
 (* Σ* *)
-Definition lang_full : Lang := Basics.const true.
+Definition lang_full : Lang := Basics.const True.
 
 (* Σ+ *)
 Definition lang_fill : Lang :=
-  fun (w : Str) => negb (eqb_str nil w).
+  fun (w : Str) => w <> empty_str.
 
-Example lang_fill_no_nil : lang_fill empty_str = false.
-Proof. reflexivity. Qed.
+Example lang_fill_no_nil : ~ lang_fill empty_str.
+Proof.
+  intro H. unfold lang_fill in H.
+  unfold not in H. apply H. reflexivity.
+Qed.
 
 Inductive Sublang (l1 l2: Lang) : Prop :=
-  include : (forall w : Str, l1 w = true -> l2 w = true) -> Sublang l1 l2.
+  include : (forall w : Str, l1 w -> l2 w) -> Sublang l1 l2.
 
 Lemma lang_full_whole : forall l : Lang, Sublang l lang_full.
 Proof.
   intro l. apply include.
-  intros w H. reflexivity.
+  intros w H. exact I.
 Qed.
 
 
 (* complement language *)
 Definition complang (l : Lang) : Lang :=
-  fun (w : Str) => negb (l w).
+  fun (w : Str) => ~ l w.
 
 Definition revlang (l : Lang) : Lang :=
   fun (w : Str) => l (reverse w).
 
-Definition catlang (l1 l2 : Lang) : Lang. Admitted.
+Definition catlang (l1 l2 : Lang) : Lang :=
+  fun (w : Str) => exists u v, l1 u /\ l2 v /\ concat u v = w.
 
-Definition timeslang (n : nat) (l : Lang) : Lang. Admitted.
+Fixpoint timeslang (n : nat) (l : Lang) (w : Str) :=
+  match n with
+  | O   => w = empty_str
+  | S n => exists u v, concat u v = w /\ l u /\ timeslang n l v
+  end.
 
-Example timeslang_0 : forall l : Lang, timeslang 0 l = list2lang [nil]. Admitted.
+Definition eq_lang (l1 l2 : Lang) := forall w, l1 w <-> l2 w.
+
+
+Example timeslang_0 : forall l : Lang, eq_lang (timeslang 0 l) (list2lang [empty_str]).
+Proof.
+  intro l. split.
+  - destruct w as [ | h t]; simpl.
+    + intro H. left. reflexivity.
+    + intro contra. inversion contra.
+  - simpl. intro H. destruct H as [H0 | F].
+    + exact (eq_sym H0).
+    + exfalso. exact F.
+Qed.
+
 
 Definition unionlang (l1 l2 : Lang) : Lang :=
-  fun (w : Str) => l1 w || l2 w.
+  fun (w : Str) => l1 w \/ l2 w.
 
-Definition star_closure (l : Lang) : Lang. Admitted.
-Definition positive_closure (l : Lang) : Lang. Admitted.
+Definition star_closure (l : Lang) : Lang :=
+  fun (w : Str) => exists n, timeslang n l w.
+
+Definition positive_closure (l : Lang) : Lang:=
+  fun (w : Str) => exists n, timeslang (S n) l w.
+
+Definition lang_anbnambm (c1 c2 : nat) : Lang :=
+  fun (w : Str) => exists n m, concat
+    (concat (times n [c1]) (times n [c2]))
+    (concat (times m [c1]) (times m [c2])) = w.
+
+Definition cat_empty_r : forall w : Str, concat w empty_str = w := @app_nil_r nat.
+Definition cat_empty_l : forall w : Str, concat empty_str w = w := @app_nil_l nat.
+
+Example anbnambm_anbn2 : forall c1 c2,
+  eq_lang (lang_anbnambm c1 c2) (timeslang 2 (lang_anbn c1 c2)).
+Proof.
+  intros c1 c2. split.
+  - intros [n [m H]].
+    exists (concat (times n [c1]) (times n [c2])).
+    exists (concat (times m [c1]) (times m [c2])).
+    split; try (exact H).
+    split; try (exists n; reflexivity).
+
+    exists (concat (times m [c1]) (times m [c2])).
+    exists empty_str.
+    split; try (rewrite cat_empty_r; reflexivity).
+    split; try (exists m; reflexivity).
+
+    unfold timeslang. reflexivity.
+
+  - intro H. destruct H as [u1 [v1 [H1 [H2 H3]]]].
+    destruct H3 as [u2 [v2 [Y1 [Y2 Y3]]]].
+    destruct H2 as [n1 H2]. destruct Y2 as [n2 Y2].
+
+    unfold timeslang in Y3. rewrite Y3 in Y1.
+    rewrite cat_empty_r in Y1. rewrite <- Y1 in H1.
+    rewrite <- H1.
+
+    exists n1, n2. rewrite <- H2. rewrite <- Y2.
+    reflexivity.
+Qed.
+
